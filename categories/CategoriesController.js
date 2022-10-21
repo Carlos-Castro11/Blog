625a95935ee58ec5eb279e91e0349701e10d2a3d
CARLOS HENRIQUE CASTRO E SOUSA <carloshenriquemg2020@gmail.com>
2022-10-21 17:56:05 -0300

    manipulando dados

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,38 @@
 const Express = require("express");
+const app = Express();
 const router = Express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
+const bodyParser = require("body-parser");
 
+// BODY PARSER
+var urlParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// ROUTES
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
+});
+
+router.post("/categories/save", urlParser, (req, res) => {
+  var title = req.body.title;
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      slug: slugify(title),
+    }).then(() => {
+      res.redirect("/");
+    });
+  } else {
+    res.redirect("/admin/categories/new");
+  }
+});
+
+router.get("/admin/categories", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/categories/index", { categories: categories });
+  });
 });
 
 module.exports = router;
