ccd57af9469794d5c1e592e1ec802dcd8790c39c
CARLOS HENRIQUE CASTRO E SOUSA <carloshenriquemg2020@gmail.com>
2022-10-28 20:46:38 -0300

    client done

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -22,7 +22,7 @@ router.post("/categories/save", urlParser, (req, res) => {
       title: title,
       slug: slugify(title),
     }).then(() => {
-      res.redirect("/");
+      res.redirect("/admin/categories");
     });
   } else {
     res.redirect("/admin/categories/new");
@@ -32,6 +32,59 @@ router.post("/categories/save", urlParser, (req, res) => {
 router.get("/admin/categories", (req, res) => {
   Category.findAll().then((categories) => {
     res.render("admin/categories/index", { categories: categories });
+  });
+});
+
+router.post("/categories/delete", urlParser, (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/categories");
+      });
+    } else {
+      res.redirect("/admin/categories");
+    }
+  } else {
+    res.redirect("/admin/categories");
+  }
+});
+
+router.get("/admin/categories/edit/:id", urlParser, (req, res) => {
+  var id = req.params.id;
+  if (isNaN(id)) {
+    res.redirect("/admin/categories");
+  }
+  Category.findByPk(id)
+    .then((category) => {
+      if (category != undefined) {
+        res.render("admin/categories/edit", { category: category });
+      } else {
+        res.redirect("/admin/categories");
+      }
+    })
+    .catch((erro) => {
+      res.redirect("/admin/categories");
+    });
+});
+
+router.post("/categories/update", urlParser, (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+
+  Category.update(
+    { title: title, slug: slugify(title) },
+    {
+      where: {
+        id: id,
+      },
+    }
+  ).then(() => {
+    res.redirect("/admin/categories");
   });
 });
 
